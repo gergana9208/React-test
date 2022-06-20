875db937f14e49ba90aa46200f84a5d194551e21
Gigatron <58364718+gergana9208@users.noreply.github.com>
2022-06-20 17:06:27 +0100

    Udemy

---
+++ build.liferay/jar/adapt-rt.js
@@ -0,0 +1,22 @@
+Liferay.Loader.define("test-react@0.1.0/adapt-rt", ['module', 'exports', 'require'], function (module, exports, require) {
+	var define = undefined;
+	var global = window;
+	{
+		var PATH_CONTEXT = Liferay.ThemeDisplay.getPathContext();
+
+		if (PATH_CONTEXT.indexOf("/") === 0) {
+			PATH_CONTEXT = PATH_CONTEXT.substr(1);
+		}
+		if (PATH_CONTEXT !== "") {
+			PATH_CONTEXT += "/";
+		}
+
+		function adaptStaticURL(url) {
+			return PATH_CONTEXT + "o/test-react-0.1.0/" + url;
+		}
+
+		module.exports = {
+			adaptStaticURL: adaptStaticURL
+		};
+	}
+});
