d96b88108a1a62977e9b4985b2a5351183eb2ae7
Gigatron <58364718+gergana9208@users.noreply.github.com>
2022-06-23 09:26:12 +0100

    add changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import Expenses from "./components/Expenses/Expenses";
 import "./components/UI/Card.css";
 import { useState } from "react";
+import "./components/Expenses/ExpensesList.css";
 import Demo from "./components/Demo/Demo";
 import NewExpense from "./components/NewExpense/NewExpense";
+
 const DUMMY_EXPENENSES = [
   {
     id: "e1",
     title: "telephone",
-    sport: 'football',
-    description:'jdisjfsdion fdivjdjv fjidfjso',
+    sport: "football",
+    description: "jdisjfsdion fdivjdjv fjidfjso",
     amount: "45.34",
     date: new Date(2020, 45, 34),
   },
   {
     id: "e2",
     title: " fridge",
-    sport: 'football',
-    description:'jdisjfsdion fdivjdjv fjidfjso',
+    sport: "football",
+    description: "jdisjfsdion fdivjdjv fjidfjso",
     amount: "30.78",
     date: new Date(2019, 4, 12),
   },
 ];
-
 
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENENSES);
@@ -34,16 +35,13 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        
         <Demo></Demo>
-       
-
+        <h2 className="expenses-list__fallback">Another sample app: </h2>
+        <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
+        <Expenses items={expenses}></Expenses>
       </header>
-
-      
     </div>
   );
 };
-
 
 export default App;
--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -5,7 +5,7 @@ const ExpensesList = props => {
     
     
     if (props.items.length === 0) {
-        return <h2 className="expenses-list__falback">Found no expenses.</h2>
+        return <h2 className="expenses-list__fallback">Found no expenses.</h2>
     }
       
 
