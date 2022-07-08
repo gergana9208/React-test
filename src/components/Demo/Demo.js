8ab5ac8e5c0264860b680ef0afed868c45b7b921
Gigatron <58364718+gergana9208@users.noreply.github.com>
2022-07-08 14:09:24 +0100

    Mc Donalds Cup Table

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -1,79 +1,116 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Card from '../UI/Card';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Card from "../UI/Card";
+import "./Demo.css";
+
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.white,
-    color: theme.palette.common.black,
-    
+    color: "#808080",
+    fontWeight: 600,
+    textAlign: "left",
+    border: "1px solid rgb(220, 214, 214)",
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
-    
+    fontWeight: 600,
+    textAlign: "left",
+    border: "1px solid rgb(220, 214, 214)",
   },
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
+  "&:nth-of-type(odd)": {
     backgroundColor: theme.palette.action.hover,
-    
+    textAlign: "left",
   },
   // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-    paddingLeft: '30px',
-  },
+  "&:last-child td, &:last-child th": {},
 }));
 
-function createData(name, sport, description) {
-  return { name, sport, description };
+function createData(name, sport, description, emptycol) {
+  return { name, sport, description, emptycol };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 'Football', 4.0),
-  createData('Ice cream sandwich', 'Tennis', 4.3),
-  createData('Eclair', 'Swimming', 6.0),
-  createData('Cupcake', 'Football', 7.9),
-  createData('Gingerbread', 'Football', 3.9),
+  createData("Mc Donald's Cup 2022", "Football", "Mc Donald's Cup 2022", ""),
+  createData("Mc Donald's Cup 2021", "Football", "Mc Donald's Cup"),
+  createData("Mc Donald's Cup 2020", "Football", "The Mc Donald's Cup"),
+  createData("Mc Donald's Cup 2019", "Football", "Mc Donald's Cup"),
+  createData(
+    "Mc Donald's Cup 2018",
+    "Football",
+    "The Return of Mc Donald's Cup"
+  ),
 ];
+
+
+
+
+
 
 export default function CustomizedTables() {
   return (
-    <li>
-      <Card className="expenses">
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Name</StyledTableCell>
-            <StyledTableCell align="right">Sport</StyledTableCell>
-            <StyledTableCell align="right">Description</StyledTableCell>
-            
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <StyledTableCell component="th" scope="row">
-                {row.name}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.sport}</StyledTableCell>
-              <StyledTableCell align="right">{row.description}</StyledTableCell>
-              
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Card className="expenses">
+      <div>
+        <h2 className="title">Mc Donalds's Cup List</h2>
+      </div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 750 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Name</StyledTableCell>
+              <StyledTableCell align="right">Sport</StyledTableCell>
+              <StyledTableCell align="right">Description</StyledTableCell>
+              <StyledTableCell align="right"></StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <StyledTableRow key={row.name}>
+                <StyledTableCell component="th" scope="row">
+                  {row.name}
+                </StyledTableCell>
+                <StyledTableCell align="right">{row.sport}</StyledTableCell>
+                <StyledTableCell align="right">
+                  {row.description}
+                </StyledTableCell>
+                <StyledTableCell align="right">
+                  {row.emptycol}
+                  <div>
+                    <button className="action" align="right">
+                      &#9660;Actions
+                    </button>
+                  </div>
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <div className="buttons">
+        <div>
+          <button className="create">Create new Cup</button>
+        </div>
+        <div>
+          <button className="choose">Choose file</button>
+        </div>
+        <div>
+          <button className="import">Import</button>
+        </div>
+      </div>
+      
+
+     
+     
     </Card>
-    </li>
   );
 }
